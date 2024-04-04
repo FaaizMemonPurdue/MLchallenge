86660003d2b17135fe48aacff4c4703c605c469d
FaaizMemonPurdue <fmemon@purdue.edu>
2024-04-04 02:25:10 +0000

    wait that one matters a lot

--- csvreader.py
+++ csvreader.py
@@ -11,7 +11,7 @@ def writeGuessCSV(guesses, out_csv="guesses.csv"): #pass in Nx1 ndarr
         writer = csv.writer(csvfile)
         writer.writerow(["Id","Category"])
         for i, elem in enumerate(guesses):
-            writer.writerow([i, names[elem[0]]])
+            writer.writerow([i, names[elem]])
 
 # samp_arr = np.array([[1], [2], [3], [4]])
 # writeGuessCSV(samp_arr, samp_csv)
